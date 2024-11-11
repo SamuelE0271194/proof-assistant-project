725a7ab14b114201e22e07e2633eb072358fb288
SamuelE0271194 <e0271194@u.nus.edu>
2024-11-11 22:48:05 +0100

    11/11 1.1-1.2, still have some doubts about the defn of tm, if I should overload the defn or not. In any case the type of the abstraction should be ty without a doubt, cause thats where the -> lives, if that makes any sense

--- simple/prover.ml
+++ simple/prover.ml
@@ -1,9 +1,38 @@
 (** Type variables. *)
 type tvar = string
-(*test*)
+
 (** Term variables. *)
 type var = string
 
 (** Types. *)
 type ty =
-  | (* Fill this in! *)
+  | Var of var
+  | Imp of ty * ty
+
+(** lambda terms *)
+type tm =
+  | Varm of var
+  | Appm of tm * tm
+  | Absm of tm * ty * tm  (* the middle term is a type not a lambda term*)
+
+let rec string_of_ty (typ : ty) =
+  match typ with
+  | Var y -> y
+  | Imp (a, b) -> "(" ^ string_of_ty a ^ " ⇒ " ^ string_of_ty b ^ ")"
+
+let rec string_of_tm (tmp : tm) =
+  match tmp with
+  | Varm y -> y
+  | Appm (a, b) -> "(" ^ string_of_tm a ^ " " ^ string_of_tm b ^ ")" 
+  | Absm (a, b, c) -> "(λ (" ^ string_of_tm a ^ " : " ^ string_of_ty b ^ ") -> " ^ string_of_tm c ^ ")"
+
+(** Test codes *)
+let () = 
+  let a = Var ("A") in
+  let b = Var ("B") in
+  let c = Var ("C") in
+  print_endline (string_of_ty (Imp (Imp (a, b), Imp(a, c))));
+  let x = Varm ("x") in
+  let f = Varm ("f") in
+  print_endline (string_of_tm (Absm (f, Imp(a, b), Absm(x, a, Appm(f, x)))));
+
