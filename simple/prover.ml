cba59a597f9919ba9d1854e92a6e5ce7d168adcc
SamuelE0271194 <e0271194@u.nus.edu>
2024-11-14 11:04:22 +0100

    removed some type checking input, and added the initial defintions of 1.4, also added some notes in the test code

--- simple/prover.ml
+++ simple/prover.ml
@@ -15,24 +15,36 @@ type tm =
   | Appm of tm * tm
   | Absm of tm * ty * tm  (* the middle term is a type not a lambda term*)
 
-let rec string_of_ty (typ : ty) =
+let rec string_of_ty typ =
   match typ with
   | Var y -> y
   | Imp (a, b) -> "(" ^ string_of_ty a ^ " ⇒ " ^ string_of_ty b ^ ")"
 
-let rec string_of_tm (tmp : tm) =
+let rec string_of_tm tmp =
   match tmp with
   | Varm y -> y
   | Appm (a, b) -> "(" ^ string_of_tm a ^ " " ^ string_of_tm b ^ ")" 
   | Absm (a, b, c) -> "(λ (" ^ string_of_tm a ^ " : " ^ string_of_ty b ^ ") -> " ^ string_of_tm c ^ ")"
 
+type context = (var * ty) list
+
+exception Type_error
+
+
+
 (** Test codes *)
+
+(* 
 let () = 
   let a = Var ("A") in
   let b = Var ("B") in
   let c = Var ("C") in
-  print_endline (string_of_ty (Imp (Imp (a, b), Imp(a, c))));
+  print_endline (string_of_ty (Imp (Imp (a, b), Imp(a, c)))); 
+  (*Input : (A → B) → A → C*) 
+  (*Expected : ((A ⇒ B) ⇒ (A ⇒ C))*)
   let x = Varm ("x") in
   let f = Varm ("f") in
   print_endline (string_of_tm (Absm (f, Imp(a, b), Absm(x, a, Appm(f, x)))));
-
+  (*Input : λ(f : A → B).λ(x : A).fx*)
+  (*Expected : (λ (f : (A ⇒ B)) -> (λ (x : A) -> (f x)))*)
+  *)
