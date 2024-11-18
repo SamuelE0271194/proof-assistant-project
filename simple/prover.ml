8750c7f795681dfa2cdd1af0385124a517c2c890
SamuelE0271194 <e0271194@u.nus.edu>
2024-11-18 16:48:29 +0100

    done till 1.11, in terms of Falm, I chose to set it to whatever, its possible to change the defn of Falm to get the type of the term you want -> Falm of tm * ty

--- simple/prover.ml
+++ simple/prover.ml
@@ -8,23 +8,50 @@ type var = string
 type ty =
   | Var of var
   | Imp of ty * ty
+  | Conj of ty * ty
+  | Truth
+  | Disj of ty * ty
+  | False
 
 (** lambda terms *)
 type tm =
   | Varm of var
   | Appm of tm * tm
   | Absm of var * ty * tm  (* the middle term is a type not a lambda term*)
+  | Pairm of tm * tm (*Conjunction*)
+  | Fstm of tm
+  | Sndm of tm
+  | Casem of tm * var * tm * var *tm (*Disjunction*)
+  | Rcasem of tm * ty
+  | Lcasem of tm * ty
+  | Trum
+  | Falm of tm
 
 let rec string_of_ty typ =
   match typ with
   | Var y -> y
   | Imp (a, b) -> "(" ^ string_of_ty a ^ " ⇒ " ^ string_of_ty b ^ ")"
+  | Conj (a, b) -> "(" ^ string_of_ty a ^ " /\\ " ^ string_of_ty b ^ ")"
+  | Disj (a, b) -> "(" ^ string_of_ty a ^ " \\/ " ^ string_of_ty b ^ ")"
+  | Truth -> "T"
+  | False -> "⊥"
 
 let rec string_of_tm tmp =
   match tmp with
   | Varm y -> y
   | Appm (a, b) -> "(" ^ string_of_tm a ^ " " ^ string_of_tm b ^ ")" 
   | Absm (a, b, c) -> "(λ (" ^ a ^ " : " ^ string_of_ty b ^ ") -> " ^ string_of_tm c ^ ")"
+  | Fstm a -> string_of_tm a
+  | Sndm b -> string_of_tm b
+  | Pairm (a, b) -> "(" ^ string_of_tm a ^ " /\\ " ^ string_of_tm b ^ ")"
+  | Casem (t, x, u, y, v) -> 
+    "case(" ^ string_of_tm t ^ ", " ^
+     x ^ "->" ^ string_of_tm u ^ ", " ^
+      y ^ "->" ^ string_of_tm v ^ ")"
+  | Rcasem (x, a) -> "(incl R, ty:" ^ string_of_ty a ^ " term: " ^ string_of_tm x ^ ")"
+  | Lcasem (x, a) -> "(incl L, ty:" ^ string_of_ty a ^ " term: " ^ string_of_tm x ^ ")"
+  | Trum -> "T"
+  | Falm a -> "⊥"
 
 type context = (var * ty) list
 
@@ -53,11 +80,47 @@ let rec infer_type ctx tm =
     let ctx_with_var = (lam, tlam) :: ctx in
     Imp (tlam, infer_type ctx_with_var fn)
   )
+  | Pairm (f, s) -> Conj (infer_type ctx f, infer_type ctx s)
+  | Fstm fst -> (
+    let tfst = infer_type ctx fst in
+    match tfst with 
+    | Conj (a, b) -> a
+    | _ -> raise (Type_error)
+  )
+  | Sndm snd -> (
+    let tsnd = infer_type ctx snd in
+    match tsnd with 
+    | Conj (a, b) -> b
+    | _ -> raise (Type_error)
+  )
+  | Trum -> Truth
+  | Casem (t, x, u, y, v) -> (
+    match infer_type ctx t with
+    | Disj (tyl, tyr) -> (
+      let ctx1 = (x, tyl) :: ctx in
+      let ctx2 = (y, tyr) :: ctx in
+      match infer_type ctx1 u with
+      | w when w = infer_type ctx2 v -> w
+      | _ -> raise (Type_error)
+    )
+    | _ -> raise (Type_error)
+  )
+  | Lcasem (tm, ty) ->
+    Disj (infer_type ctx tm, ty)
+  | Rcasem (tm, ty) -> 
+    Disj (ty, infer_type ctx tm)
+  | Falm tm -> 
+    match infer_type ctx tm with
+    | False -> Var "Anything"
+    | _ -> raise (Type_error)
 
 and check_type ctx tm ty =
-  match infer_type ctx tm with 
-  | x when x = ty -> ()
-  | _ -> raise (Type_error)
+  match tm with 
+  | Falm t -> check_type ctx t False
+  | _ ->
+    match infer_type ctx tm with 
+    | x when x = ty -> ()
+    | _ -> raise (Type_error)
 
 (** Test codes *)
 let () = 
@@ -89,20 +152,20 @@ let () =
   try 
     let ctx = [] in
     let temp = infer_type ctx tyer1 in
-    print_endline ("This should be an error");
+    print_endline ("This should be an error " ^ string_of_ty temp);
   with
   | Type_error -> print_endline("Type Error with tyer1");
   try 
     let ctx = [] in
     let temp = infer_type ctx tyer2 in
-    print_endline ("This should be an error");
+    print_endline ("This should be an error " ^ string_of_ty temp);
   with
   | Type_error -> print_endline("Type Error with tyer2");
 
  try 
     let ctx = [] in
     let temp = (infer_type ctx tyer3) in
-    print_endline("no problems");
+    print_endline("This should be an error " ^ string_of_ty temp);
   with
   | Type_error -> print_endline("Type Error with tyer3");
   (*Expecting gfx to pass and Tyer = Type error to fail*)
@@ -110,24 +173,44 @@ let () =
   (*1.5*)
   let ctx = [] in
   let checkty = Absm(x, a, Varm x) in
-  let temp = check_type ctx checkty (Imp(a, a)) in
+  check_type ctx checkty (Imp(a, a));
   print_endline ("check is type A -> A");
  
   try 
     let ctx = [] in
-    let temp = check_type ctx checkty (Imp(b, b)) in 
+    check_type ctx checkty (Imp(b, b));
     print_endline ("check has type B -> B (this should be an error)");
   with
   | Type_error -> print_endline("check is not type B -> B");
  
   try 
     let ctx = [] in
-    let temp = check_type ctx (Varm x) a in 
+    check_type ctx (Varm x) a;
     print_endline ("x has type A (this should be an error)");
   with
-  | Type_error -> print_endline("x is not type A");
- 
+  | Type_error -> print_endline("x is not type A"); 
 
+  (*1.8*)
+  let and_comm = Absm (x, Conj(a, b), Pairm (Sndm (Varm x), Fstm (Varm x))) in
+  print_endline (string_of_ty (infer_type [] and_comm));
+
+  (*1.9*)
+  let t_a_a = Absm (f, Imp (Truth, a), Appm(Varm f, Trum)) in
+  print_endline (string_of_ty (infer_type [] t_a_a));
+ 
+  (*1.10*)
+  let disj_comm = Absm (x, Disj(a, b), 
+                    Casem (Varm x, 
+                      "u", Rcasem (Varm "u", b),
+                      "v", Lcasem (Varm "v", a))) in
+  print_endline (string_of_ty (infer_type [] disj_comm));
+
+  (*1.11*)
+  let a_and_a_false = Absm (x, Conj(a, Imp(a, False)), 
+                      Falm (Appm (Sndm (Varm x), Fstm (Varm x)))) in
+  print_endline (string_of_ty (infer_type [] a_and_a_false));  
+  (*You could get the final bit to say B instead of Anything,
+   but you have force the output of Falm -> the term needs a type too*)
 
 
 
