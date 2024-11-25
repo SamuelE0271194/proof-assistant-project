edea1abc76835562049ef3f3c6dc229c98d14d12
SamuelE0271194 <e0271194@u.nus.edu>
2024-11-25 21:25:53 +0100

    moved some comments into a new file (testPart1.ml) and don't forget to use test ( test) if you want to run

--- simple/prover.ml
+++ simple/prover.ml
@@ -137,138 +137,6 @@ and check_type ctx tm ty =
     | x when x = ty -> Unitm
     | _ -> raise (Type_error)
 
-(** Test codes *)
-(*
-let () = 
-  
- (*Constants that are used in some tests*)
-  let a = Var ("A") in
-  let b = Var ("B") in
-  let c = Var ("C") in
-  
-  let x = ("x") in
-  let f = ("f") in
-  let g = ("g") in
-  
-  (*1.3*) 
-  print_endline (string_of_ty (Imp (Imp (a, b), Imp(a, c)))); 
-  (*Input : (A → B) → A → C*) 
-  (*Expected : ((A ⇒ B) ⇒ (A ⇒ C))*)
-  print_endline (string_of_tm (Absm (f, Imp(a, b), Absm(x, a, Appm(Varm f, Varm x)))));
-  (*Input : λ(f : A → B).λ(x : A).fx*)
-  (*Expected : (λ (f : (A ⇒ B)) -> (λ (x : A) -> (f x)))*)
-  
-  (*1.4*)
-  let gfx = Absm (f, Imp(a, b), Absm(g, Imp(b, c), Absm(x, a, Appm(Varm g, Appm(Varm f, Varm x))))) in 
-  let ctx = [] in
-  print_endline (string_of_ty (infer_type ctx gfx));
-  let tyer1 = Absm (f, a, Varm x) in
-  let tyer2 = Absm (f, a, Absm(x, b, Appm(Varm f, Varm x))) in
-  let tyer3 = Absm (f, Imp(a, b), Absm(x, b, Appm(Varm f, Varm x))) in
-  try 
-    let ctx = [] in
-    let temp = infer_type ctx tyer1 in
-    print_endline ("This should be an error " ^ string_of_ty temp);
-  with
-  | Type_error -> print_endline("Type Error with tyer1");
-  try 
-    let ctx = [] in
-    let temp = infer_type ctx tyer2 in
-    print_endline ("This should be an error " ^ string_of_ty temp);
-  with
-  | Type_error -> print_endline("Type Error with tyer2");
-
- try 
-    let ctx = [] in
-    let temp = (infer_type ctx tyer3) in
-    print_endline("This should be an error " ^ string_of_ty temp);
-  with
-  | Type_error -> print_endline("Type Error with tyer3");
-  (*Expecting gfx to pass and Tyer = Type error to fail*)
-
-  (*1.5*)
-  let ctx = [] in
-  let checkty = Absm(x, a, Varm x) in
-  let temp = check_type ctx checkty (Imp(a, a)) in
-  print_endline ("check is type A -> A" ^ string_of_tm temp);
- 
-  try 
-    let ctx = [] in
-    let temp = check_type ctx checkty (Imp(b, b)) in
-    print_endline ("check has type B -> B (this should be an error)" ^ string_of_tm temp);
-  with
-  | Type_error -> print_endline("check is not type B -> B");
- 
-  try 
-    let ctx = [] in
-    let temp = check_type ctx (Varm x) a in 
-    print_endline ("x has type A (this should be an error)" ^ string_of_tm temp);
-  with
-  | Type_error -> print_endline("x is not type A"); 
-
-  (*1.8*)
-  let and_comm = Absm (x, Conj(a, b), Pairm (Sndm (Varm x), Fstm (Varm x))) in
-  print_endline (string_of_ty (infer_type [] and_comm));
-
-  (*1.9*)
-  let t_a_a = Absm (f, Imp (Truth, a), Appm(Varm f, Trum)) in
-  print_endline (string_of_ty (infer_type [] t_a_a));
- 
-  (*1.10*)
-  let disj_comm = Absm (x, Disj(a, b), 
-                    Casem (Varm x, 
-                      "u", Rcasem (Varm "u", b),
-                      "v", Lcasem (Varm "v", a))) in
-  print_endline (string_of_ty (infer_type [] disj_comm));
-
-  (*1.11*)
-  let a_and_a_false = Absm (x, Conj(a, Imp(a, False)), 
-                      Falm (Appm (Sndm (Varm x), Fstm (Varm x)), b)) in
-  print_endline (string_of_ty (infer_type [] a_and_a_false));  
-*)
-(*
-let () =
-  let l = [
-    "A => B";
-    "A ⇒ B";
-    "A /\\ B";
-    "A ∧ B";
-    "T";
-    "A \\/ B";
-    "A ∨ B";
-    "_";
-    "not A";
-    "¬ A";
-  ]
-  in
-  List.iter
-    (fun s ->
-       Printf.printf
-         "the parsing of %S is %s\n%!" s (string_of_ty (ty_of_string s))
-    ) l
-
-let () =
-  let l = [
-    "t u v";
-    "fun (x : A) -> t";
-    "λ (x : A) → t";
-    "(t , u)";
-    "fst(t)";
-    "snd(t)";
-    "()";
-    "case t of x -> u | y -> v";
-    "left(t,B)";
-    "right(A,t)";
-    "absurd(t,A)"
-  ]
-  in
-  List.iter
-    (fun s ->
-       Printf.printf
-         "the parsing of %S is %s\n%!" s (string_of_tm (tm_of_string s))
-    ) l
-*)
-
 (*Part 2*)
 let rec string_of_ctx ctx =
   match ctx with 
@@ -281,6 +149,7 @@ type sequent = context * ty
 let string_of_seq seq =
   match seq with
   | (ctx, ty) -> string_of_ctx ctx ^ " |- " ^ string_of_ty ty
+
 (*Tests for string of ctx and seq
 let () = 
   let a = Var "A" in 
