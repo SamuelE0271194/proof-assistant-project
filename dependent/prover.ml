4b4a91c99713976c9ff5ff0db4129d250dba19e2
SamuelE0271194 <e0271194@u.nus.edu>
2024-12-03 13:56:26 +0100

    i suppose that 3 is correct, updated to 5.3 fresh variable

--- dependent/prover.ml
+++ dependent/prover.ml
@@ -16,4 +16,17 @@ type expr =
   | Refl of expr
   | J of expr * expr * expr * expr * expr
 
-(* Fill me in! *)
+let rec to_string exp =
+  match exp with
+  | Type -> "Type"
+  | Var x -> x
+  | App (a, b) -> "(" ^ to_string a ^ " => " ^ to_string b ^ ")"
+  | Abs (a, b, c) -> "(fun (" ^ a ^ " : " ^ to_string b ^ ") -> " ^ to_string c ^ ")"
+  | Pi (a, b, c) -> "Pi(" ^ a ^ ", " ^ to_string b ^ ", " ^ to_string c ^ ")"
+  | _ -> "Not implemented yet"
+
+let fresh_constant = ref 0 ;;
+
+let fresh_var _ = 
+  fresh_constant := !fresh_constant + 1;
+  Var ("x" ^ string_of_int(!fresh_constant))
