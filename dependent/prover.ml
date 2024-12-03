ee2ef8a58f21505ba83be0acf1e98e06947fc7e0
SamuelE0271194 <e0271194@u.nus.edu>
2024-12-03 17:30:40 +0100

    updated to 5.5

--- dependent/prover.ml
+++ dependent/prover.ml
@@ -29,4 +29,37 @@ let fresh_constant = ref 0 ;;
 
 let fresh_var _ = 
   fresh_constant := !fresh_constant + 1;
-  Var ("x" ^ string_of_int(!fresh_constant))
+  ("x" ^ string_of_int(!fresh_constant))
+
+let rec subst x ex1 ex2 =
+  match ex2 with 
+  | Type -> Type
+  | Var var when var = x -> ex1
+  | App (exA, exB) -> App (subst x ex1 exA, subst x ex1 exB)
+  | Abs (var, exA, exB) -> (
+    match var with
+    | y when y = x -> Abs (x, exA, subst x ex1 exB)
+    | _ -> 
+      match ex2 with
+      | Var z when z = var ->
+        let new_var = fresh_var () in 
+        let new_Var = Var new_var in
+        Abs (new_var, exA, subst x ex1 (subst z new_Var ex2))
+      | _ -> Abs (var, exA, subst x ex1 exB)
+  )
+  | Pi (var, exA, exB) -> (
+    match var with
+    | y when y = x -> Pi (x, exA, exB)
+    | _ -> Pi (x, subst x ex1 exA, subst x ex1 exB)
+  )
+  | _ -> Type (*"not yet implemented"*)
+
+type context = (var * (expr * expr option)) list
+
+let rec string_of_context ctx =
+  match ctx with
+  | [] -> ""
+  | (var, (exp1)) :: l -> 
+    var ^ " : " ^ to_string exp1 ^ "\n" ^ string_of_context l
+  | (var, (exp1, exp2)) :: l -> 
+    var ^ " : " ^ to_string exp1 ^ " = " ^ to_string exp2 ^ "\n" ^ string_of_context l
